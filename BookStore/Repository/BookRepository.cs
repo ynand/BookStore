a91732a924ab2aebfdce5a5061439efa9c5e9442
Nandaja <yarramareddynandaja@gmail.com>
2021-01-06 15:36:17 +0530

    Added Models, View, Jquery and bootstarp

---
+++ BookStore/Repository/BookRepository.cs
@@ -0,0 +1,37 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+        public List<BookModel> SearchBook(string title,string authorName)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) || x.Author.Contains(authorName)).ToList();
+        }
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){Id=1,Title="Dinosaurs", Author="Ross"},
+                new BookModel(){Id=2,Title="Fashion", Author="Rachel"},
+                new BookModel(){Id=3,Title="Data Processing", Author="Chandler"},
+                new BookModel(){Id=4,Title="Food", Author="Monika"},
+                new BookModel(){Id=5,Title="Acting", Author="Joey"},
+                new BookModel(){Id=6,Title="Massage", Author="Phoebe"},
+                new BookModel(){Id=7,Title="Dinosaurs", Author="Ross"}
+            };
+        }
+    }
+}
--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -26,7 +28,8 @@ namespace BookStore
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseStaticFiles();
+            
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
